c8537c25831c0a40e6aff937ccbaf8e6b390bd94
ggdream <gdream@yeah.net>
2021-07-28 10:12:16 +0800

    移除event_bus包

--- utils/server.dart
+++ utils/server.dart
@@ -1,30 +1,30 @@
-import 'dart:io';
+// import 'dart:io';
 
-import 'package:dia/dia.dart';
-import 'package:dia_body/dia_body.dart';
-import 'package:dia_cors/dia_cors.dart';
-import 'package:dia_router/dia_router.dart';
-import 'package:dia_static/dia_static.dart';
+// import 'package:dia/dia.dart';
+// import 'package:dia_body/dia_body.dart';
+// import 'package:dia_cors/dia_cors.dart';
+// import 'package:dia_router/dia_router.dart';
+// import 'package:dia_static/dia_static.dart';
 
-class ContextWithRoutingBody extends Context with Routing, ParsedBody {
-  ContextWithRoutingBody(HttpRequest request) : super(request);
-}
+// class ContextWithRoutingBody extends Context with Routing, ParsedBody {
+//   ContextWithRoutingBody(HttpRequest request) : super(request);
+// }
 
-void main() {
-  final app = App((request) => ContextWithRoutingBody(request));
+// void main() {
+//   final app = App((request) => ContextWithRoutingBody(request));
 
-  app.use(cors());
-  app.use(body());
-  app.use(serve('H:/code/images', prefix: '/static'));
-  app.use(genRouter().middleware);
+//   app.use(cors());
+//   app.use(body());
+//   app.use(serve('H:/code/images', prefix: '/static'));
+//   app.use(genRouter().middleware);
 
-  app.listen('127.0.0.1', 9999).then((value) => print('Server starting...'));
-}
+//   app.listen('127.0.0.1', 9999).then((value) => print('Server starting...'));
+// }
 
-Router genRouter() {
-  final router = Router('/');
-  router.get('/home', (ctx, next) async {
-    ctx.body = 'hello';
-  });
-  return router;
-}
+// Router genRouter() {
+//   final router = Router('/');
+//   router.get('/home', (ctx, next) async {
+//     ctx.body = 'hello';
+//   });
+//   return router;
+// }
