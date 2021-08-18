71f4fb13146bc7e4244c8aca6d5e26d5406e49f1
ggdream <gdream@yeah.net>
2021-08-18 19:58:02 +0800

    视频页

--- lib/pages/video/live/live.dart
+++ lib/pages/video/live/live.dart
@@ -1,4 +1,4 @@
-import 'package:fijkplayer/fijkplayer.dart';
+// import 'package:fijkplayer/fijkplayer.dart';
 import 'package:flutter/material.dart';
 
 class VideoLiveWatchPage extends StatefulWidget {
@@ -8,20 +8,57 @@ class VideoLiveWatchPage extends StatefulWidget {
   _VideoLiveWatchPageState createState() => _VideoLiveWatchPageState();
 }
 
-class _VideoLiveWatchPageState extends State<VideoLiveWatchPage> {
-  final player = FijkPlayer()
-    ..setDataSource('rtmp://101.34.2.166:1935/live/movie', autoPlay: true);
+class _VideoLiveWatchPageState extends State<VideoLiveWatchPage>
+    with SingleTickerProviderStateMixin {
+  // final player = FijkPlayer()
+  //   ..setDataSource('rtmp://101.34.2.166:1935/live/movie', autoPlay: true);
+
+  late final TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 3, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: FijkView(player: player),
+      body: Column(
+        children: [
+          // FijkView(player: player),
+          Container(
+            width: double.infinity,
+            height: 240,
+            child: Row(),
+          ),
+          TabBar(
+            controller: _controller,
+            tabs: [
+              Tab(text: '聊天'),
+              Tab(text: '主播'),
+              Tab(text: '其他'),
+            ],
+          ),
+          Expanded(
+            child: TabBarView(
+              controller: _controller,
+              children: [
+                Text('data'),
+                Text('data'),
+                Text('data'),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   @override
   void dispose() {
-    player.release();
+    // player.release();
     super.dispose();
   }
 }
--- lib/pages/video/page.dart
+++ lib/pages/video/page.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'package:uni_party/styles/styles.dart';
+import 'package:uni_party/tools/sharex/sharex.dart';
 
 import 'video_player.dart';
 
@@ -14,10 +16,10 @@ class VideoPage extends StatefulWidget {
 
 class _VideoPageState extends State<VideoPage> {
   final res = [
-    // 'https://www.mocas.icu/beauty/1.mp4',
-    // 'https://www.mocas.icu/beauty/2.mp4',
-    // 'https://www.mocas.icu/beauty/3.mp4',
-    // 'https://www.mocas.icu/beauty/4.mp4',
+    'https://www.mocas.icu/beauty/1.mp4',
+    'https://www.mocas.icu/beauty/2.mp4',
+    'https://www.mocas.icu/beauty/3.mp4',
+    'https://www.mocas.icu/beauty/4.mp4',
   ];
   @override
   Widget build(BuildContext context) {
@@ -26,31 +28,44 @@ class _VideoPageState extends State<VideoPage> {
         child: Stack(
           children: [
             videoView(),
-            Align(
-              alignment: Alignment.topRight,
-              child: IconButton(
-                onPressed: () {
-                  Get.toNamed('/video/live/watch');
-                },
-                icon: Icon(Icons.camera),
-              ),
+            topActionsView(),
+            bottomVideoInfoView(),
+            rightActionsVIew()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Align rightActionsVIew() {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: Padding(
+        padding: const EdgeInsets.only(right: 8),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            avatarView(),
+            SizedBox(height: 40),
+            // 点赞
+            BrandWidget(
+              icon: Icons.favorite_rounded,
+              brand: '25',
+              onClick: () {},
             ),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    '两个',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Text(
-                    '视频简介',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
-              ),
+            SizedBox(height: 12),
+            // 评论
+            BrandWidget(
+              icon: CupertinoIcons.chat_bubble_text_fill,
+              brand: '25',
+              onClick: () {},
+            ),
+            SizedBox(height: 16),
+            // 更多操作
+            IconButton(
+              iconSize: 40,
+              onPressed: _clickMore,
+              icon: Icon(CupertinoIcons.ellipsis_circle_fill),
             ),
           ],
         ),
@@ -58,9 +73,124 @@ class _VideoPageState extends State<VideoPage> {
     );
   }
 
+  /// 视频作者的头像
+  Widget avatarView() {
+    return Container(
+      width: 64,
+      height: 64,
+      decoration: BoxDecoration(
+        border: Border.all(width: 2, color: Colors.black),
+        shape: BoxShape.circle,
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(
+            'https://www.mocas.icu/image/875.jpg',
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget topActionsView() {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(CupertinoIcons.search),
+              tooltip: '搜索',
+            ),
+            Spacer(),
+            IconButton(
+              onPressed: () => Get.toNamed('/video/live/watch'),
+              icon: Icon(CupertinoIcons.videocam_circle_fill),
+              tooltip: '直播',
+            ),
+            IconButton(
+              onPressed: () => {},
+              icon: Icon(Icons.camera),
+              tooltip: '动态',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// 视频相关元信息
+  Widget bottomVideoInfoView() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Padding(
+        padding: const EdgeInsets.only(
+          bottom: 8,
+          left: 12,
+          right: 12,
+        ),
+        child: Row(
+          children: [
+            labelView(),
+            // 可以放其他信息
+          ],
+        ),
+      ),
+    );
+  }
+
+  // 视频作者与标题
+  Widget labelView() {
+    return Flexible(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          labelTopView(),
+          Text(
+            '视频简介',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget labelTopView() {
+    return Row(
+      children: [
+        Text(
+          '魔咔啦咔',
+          style: TextStyle(fontSize: 24),
+        ),
+        SizedBox(width: 8),
+        ClipOval(
+          child: Image.network(
+            'https://pic2.zhimg.com/v2-bd165afbe15d520e279ef4354fc02186_r.jpg?source=172ae18b',
+            width: 24,
+            height: 24,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<void> _clickMore() async {
+    await showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return _ButtomActionsSheetView();
+      },
+    );
+  }
+
   Widget videoView() {
     return PageView.builder(
       physics: ScrollX.physics,
+      scrollDirection: Axis.vertical,
       itemCount: res.length,
       itemBuilder: (context, index) {
         return VideoPlayer(
@@ -68,6 +198,118 @@ class _VideoPageState extends State<VideoPage> {
           video: res[index],
         );
       },
+    );
+  }
+}
+
+/// 底部操作弹窗
+class _ButtomActionsSheetView extends StatelessWidget {
+  const _ButtomActionsSheetView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData(
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+      ),
+      child: Container(
+        height: 160,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: _onNotInterest,
+              iconSize: 48,
+              icon: Icon(CupertinoIcons.bolt_circle_fill),
+              tooltip: '不感兴趣',
+            ),
+            IconButton(
+              onPressed: _onDownload,
+              iconSize: 48,
+              icon: Icon(CupertinoIcons.arrow_down_circle_fill),
+              tooltip: '下载',
+            ),
+            IconButton(
+              onPressed: () => _onCollect(context),
+              iconSize: 48,
+              icon: Icon(CupertinoIcons.star_circle_fill),
+              tooltip: '收藏',
+            ),
+            IconButton(
+              onPressed: _onShare,
+              iconSize: 48,
+              icon: Icon(CupertinoIcons.arrowshape_turn_up_right_circle_fill),
+              tooltip: '分享',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// 不感兴趣
+  Future<void> _onNotInterest() async {}
+
+  /// 下载视频
+  Future<void> _onDownload() async {}
+
+  /// 收藏视频
+  Future<void> _onCollect(BuildContext context) async {
+    Navigator.of(context).pop();
+  }
+
+  /// 分享视频
+  Future<void> _onShare() async {
+    await ShareX.text('data');
+  }
+}
+
+/// 图标牌子
+class BrandWidget extends StatelessWidget {
+  const BrandWidget({
+    Key? key,
+    required this.icon,
+    required this.brand,
+    this.color,
+    this.textColor,
+    this.onClick,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String brand;
+
+  final Color? color;
+  final Color? textColor;
+
+  final void Function()? onClick;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        InkWell(
+          hoverColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          splashColor: Colors.transparent,
+          onTap: onClick,
+          customBorder: CircleBorder(),
+          child: Icon(
+            icon,
+            color: color,
+            size: 40,
+          ),
+        ),
+        Text(
+          brand,
+          style: TextStyle(color: textColor),
+        ),
+      ],
     );
   }
 }
